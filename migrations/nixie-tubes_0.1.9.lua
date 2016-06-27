f0265c1ee16c9e86c5db83e6e2b30a8de442f580
Thomas Deal <justarandomgeek@gmail.com>
2016-06-27 15:39:58 -0400

    Fix 0.1.9 migration file to apply cleanly to a save that has never has the mod installed.

--- migrations/nixie-tubes_0.1.9.lua
+++ migrations/nixie-tubes_0.1.9.lua
@@ -1,6 +1,8 @@
 for _,force in pairs(game.forces) do
-  force.technologies['cathodes'].reload()
-  if force.technologies["electric-energy-distribution-1"].researched then 
-   force.recipes["nixie-tube-small"].enabled = true
+  if force.technologies['cathodes'] then
+    force.technologies['cathodes'].reload()
+    if force.technologies["electric-energy-distribution-1"].researched then 
+      force.recipes["nixie-tube-small"].enabled = true
+    end
   end
 end
